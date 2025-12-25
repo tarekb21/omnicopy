f563d4cc2ae657608576d061deb717c8522dabee
Tarek Al Bouhairi <tarek.tbouhairi@gmail.com>
2025-12-25 18:40:52 +0100

    setup-database

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-from datetime import datetime
+from contextlib import asynccontextmanager
+from datetime import datetime, timezone
 from random import randint
-from fastapi import FastAPI, HTTPException, Response
-from typing import Any
+from fastapi import Depends, FastAPI, HTTPException, Response
+from typing import Annotated, Any
 
-app = FastAPI(root_path="/api/v1")
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), index=True, nullable=True)
+
+sqllite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqllite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+    
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Campaign)).first():
+            session.add_all([
+                Campaign(name="Summer Launch", due_date=datetime.now()),
+                Campaign(name="Black Friday", due_date=datetime.now())
+            ])
+            session.commit()
+    yield
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
 @app.get("/")
 async def root():
